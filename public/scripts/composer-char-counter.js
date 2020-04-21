13b76e830bd2ac816b7c3dad1b0bf7427adf09e8
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-21 19:46:41 +0000

    implementing the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+// This file will be solely responsible for this character counter.
+
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function() {
+    const maxCharacter = 140;
+    const txtAreaLength = this.value.length;
+    const totalCharacter = maxCharacter - txtAreaLength;
+    if (totalCharacter < 0){
+      $('.counter').text(totalCharacter).css("color", "#FF0000");
+    } else {
+      $('.counter').text(totalCharacter).css("color", "#545149");;
+    }
+  });
+}); 
