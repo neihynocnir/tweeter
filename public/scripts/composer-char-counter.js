344d43ca83a67e02feffd24a00c277227013f736
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-22 17:50:53 +0000

    after code review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
     const maxCharacter = 140;
     const txtAreaLength = this.value.length;
     const totalCharacter = maxCharacter - txtAreaLength;
+    const counter = $(this).closest('form').find('.counter');
     if (totalCharacter < 0){
-      $('.counter').text(totalCharacter).css("color", "#FF0000");
+      counter.text(totalCharacter).css("color", "#FF0000");
     } else {
-      $('.counter').text(totalCharacter).css("color", "#545149");;
+      counter.text(totalCharacter).css("color", "#545149");;
     }
   });
 }); 
